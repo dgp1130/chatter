0e660e91d7bf94cefed720cfe13b8adae7b53f96
Douglas Parker <dgp1130422@gmail.com>
2019-05-27 21:44:30 -0700

    Fixing material icons.
    Just needed to manually add the JSON links. Copied this from:
    https://github.com/flutter/samples/blob/cb5036305ea278e53be742c555442b62b5327881/web/vision_challenge/web/assets/FontManifest.json

--- server/server.go
+++ server/server.go
@@ -12,6 +12,7 @@ func main() {
 
 	s := gin.Default()
 	s.Use(static.Serve("/", static.LocalFile("./client/build", false)))
+	s.Use(static.Serve("/assets", static.LocalFile("./client/assets", false)))
 
 	s.Run(":8080")
 }
