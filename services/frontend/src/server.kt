e7e4e23ae8ade5d0c8e328b4945b11978947c1ef
Douglas Parker <dgp1130422@gmail.com>
2019-08-11 19:12:05 -0700

    Adding Kotlin frontend server build skeleton.
    While attempting to split the Go server into multiple services, I found that having many Go modules in a single repository is quite difficult. Particularly since they should share a separate module (the models/ directory).
    
    It appears possible to use multiple Go modules in a single repository, however it would likely be quite difficult and have lasting impact on the file structure. I also don't want to separate out each service into its own repository becuase I would have to maintain them separately, set up Travis for each, depend on any shared code, and so on.
    
    After being so frustrated with Go's module system, I decided to switch to a different language which was more conducive to this structure. Kotlin is a fairly modern language and it's package system should allow me flexibility about where I put my code, as long as it is all included in the build process.
    
    This commit simply makes a "Hello World" program. Next step is to make a simple server, and then load the client into it.

---
+++ services/frontend/src/server.kt
@@ -0,0 +1,10 @@
+package chatter.services.frontend;
+
+class Server {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello, World!")
+        }
+    }
+}
