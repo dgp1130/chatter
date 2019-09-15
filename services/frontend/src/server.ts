22bdf31db4c980419c7a9b3847b498a5c4a005d0
Douglas Parker <dgp1130422@gmail.com>
2019-09-14 22:49:09 -0700

    Updating frontend server to serve static client files.
    This mostly copies the existing monolithic Dockerfile into the frontend service to build the client and then serve it as static content.
    
    This server does not include the API functionality, because that will be handled by separate microservices.

--- services/frontend/src/server.ts
+++ services/frontend/src/server.ts
@@ -2,9 +2,8 @@ import * as express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.static('/chatter/client/build/'));
+app.use('/assets/', express.static('/chatter/client/assets/'));
 
 const port = process.env.PORT || 80;
 app.listen(port, () => {
