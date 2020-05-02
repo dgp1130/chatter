451acc19bae174de75946fc718373ce480909151
Doug Parker <dgp1130@users.noreply.github.com>
2020-05-01 17:29:58 -0700

    Update to Angular 9.1.4.
    I'm hoping this will address some of the security warnings issued by GitHub.
    
    Generated this commit by running:
    $ (cd services/ng-frontend/ && ng update --all)
    
    This actually took a couple attempts, due to a bug in the CLI. Filed https://github.com/angular/angular-cli/issues/17599 to look into it further.

--- services/ng-frontend/src/app/app.module.ts
+++ services/ng-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
